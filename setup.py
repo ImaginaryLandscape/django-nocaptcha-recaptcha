cfd21bdaf9544eb77679e4ac5c93f0476dd3875f
Joe Jasinski <jjasinski@imagescape.com>
2014-12-10 12:12:39 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django-nocaptcha-recaptcha',
-    version='0.0.5',
+    version='0.0.6',
     description='Django nocaptcha recaptcha form field/widget app.',
     long_description=open('README.md', 'r').read(),
     author='Imaginary Landscape',
