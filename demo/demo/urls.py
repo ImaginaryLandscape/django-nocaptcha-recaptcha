e986f4d0b3d7a5bfa70660f44d46086a17e051c5
Joe Jasinski <joe.jasinski@gmail.com>
2017-02-06 17:33:53 -0600

    update demo project to test django1.9 and 1.10 support

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from . import views
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
 
     url(r'^$', views.DemoView.as_view(template_name="index.html"), {},
         name="index"),
@@ -16,4 +15,4 @@ urlpatterns = patterns(
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+]
